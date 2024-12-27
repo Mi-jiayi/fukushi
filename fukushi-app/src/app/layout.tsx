3194845c10fabe041058d01ea9f32e0f350107fb
Mi-jiayi <kingyomjy@gmail.com>
2024-12-27 17:33:44 +0900

    header 作成

--- fukushi-app/src/app/layout.tsx
+++ fukushi-app/src/app/layout.tsx
@@ -26,24 +26,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="bg-gray-100">
-          <header className="bg-gray-200 p-4 flex justify-between items-center">
-            <a href="#" className="text-gray-800 font-semibold">← 一覧に戻る</a>
-            <div className="flex items-center">
-              <span className="text-gray-800">account</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 ml-2 text-gray-800"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 10a3 3 0 100-6 3 3 0 000 6zM2 17a8 8 0 1116 0H2z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-          </header>
           {children}
         </div>
       </body>
--- fukushi-app/src/app/page.tsx
+++ fukushi-app/src/app/page.tsx
@@ -1,9 +1,14 @@
-
-export default function Home() {
+import { IoPersonOutline } from "react-icons/io5";
+// 
+export default function KanJaList() {
   return (
   <div>
-    123
+    <header className="bg-gray-200 p-4 flex justify-end items-center">
+      <div className="flex items-center bg-white px-4 py-2 rounded-lg shadow">
+        <IoPersonOutline className="text-gray-800 w-5 h-5" />
+        <span className="text-gray-800 ml-2 font-medium">account</span>
+      </div>
+    </header>
   </div>
-    
   );
 }
